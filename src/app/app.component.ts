b91864adcde44ac1194dac837738546407efd9e0
Antonio Gantolea <antoniogantolea@yahoo.it>
2024-10-21 12:12:17 +0200

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+  todos: any[] = [];
+  filteredTodos: any[] = [];
+  userIds: number[] = [];
+  filterForm: FormGroup;
+
+  constructor(private todoService: TodoService, private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      searchText: [''],
+      completedFilter: [null],
+      selectedUserIds: [[]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe(data => {
+      this.todos = data;
+      this.filteredTodos = this.todos;
+      this.userIds = [...new Set(this.todos.map(todo => todo.userId))];
+      this.filterForm.valueChanges.subscribe(() => {
+        this.applyFilters();
+      });
+    });
+  }
+
+  applyFilters() {
+    const { searchText, completedFilter, selectedUserIds } = this.filterForm.value;
+
+    this.filteredTodos = this.todos.filter(todo => {
+      const matchesSearch = searchText
+        ? todo.title.toLowerCase().includes(searchText.toLowerCase())
+        : true;
+      const matchesCompleted = completedFilter === null
+        ? true
+        : todo.completed === completedFilter;
+      const matchesUser = selectedUserIds.length === 0
+        ? true
+        : selectedUserIds.includes(todo.userId);
+
+      return matchesSearch && matchesCompleted && matchesUser;
+    });
+  }
+
+  resetFilters() {
+    this.filterForm.reset({
+      searchText: '',
+      completedFilter: null,
+      selectedUserIds: []
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
